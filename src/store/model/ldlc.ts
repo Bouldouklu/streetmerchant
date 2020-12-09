03e0c10c67a63b8530c7a018ca14d583c6b69de3
Bouldouklu <75644130+Bouldouklu@users.noreply.github.com>
2020-12-09 13:07:58 +0100

    Create ldlc.ts

---
+++ src/store/model/ldlc.ts
@@ -0,0 +1,37 @@
+import {Store} from './store';
+
+export const ldlc: Store = {
+	labels: {
+		inStock: {
+			container: 'a.button.picto.color2.noMarge.add-to-cart',
+			text: ['']
+		},
+		maxPrice: {
+			container: 'div.price'
+		}
+	},
+	links: [
+		{
+			brand: 'test:brand',
+			model: 'test:model',
+			series: 'test:series',
+			url:
+				'https://www.ldlc.com/fiche/PB00276527.html'
+		},
+		{
+			brand: 'nvidia',
+			model: 'founders edition',
+			series: '3060ti',
+			url:
+				'https://www.ldlc.com/fiche/PB00394371.html'
+		},
+    	{
+			brand: 'nvidia',
+			model: 'founders edition',
+			series: '3080',
+			url:
+				'https://www.ldlc.com/fiche/PB00276527.html'
+		},
+	],
+	name: 'ldlc'
+};
